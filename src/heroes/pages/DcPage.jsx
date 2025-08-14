b93632c3690b30ef2ebe77395d05aa2a9091374e
Robinson Betancur Marin <rbetancurm@ultragroupla.com>
2025-08-14 10:23:44 -0500

    first commit

---
+++ src/heroes/pages/DcPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { HeroList } from "../components";
+
+export const DcPage = () => {
+  return (
+    <>
+      <h1>Dc Comics</h1>
+      <hr />
+
+      <HeroList publisher={"DC Comics"} />
+    </>
+  );
+};
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router";
+
+import { LoginPage } from "../auth";
+import { HeroesRoutes } from "../heroes";
+
+
+export const AppRouter = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="login" element={<LoginPage />} />
+
+        <Route path="/*" element={<HeroesRoutes />} />
+      </Routes>
+    </>
+  );
+};
